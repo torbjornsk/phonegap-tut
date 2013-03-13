19ff0043983e6c7a3a40b0cf6050c9fbf3662eb2
Torbjorn Skyberg Knutsen <torbjornk@mesan.no>
2013-03-13 16:06:39 +0100

    kamera

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -11,6 +11,7 @@ var EmployeeView = function(employee) {
     var that = this;
       this.el = $('<div/>');
       this.el.on('click', '.add-location-btn', function() {that.addLocation(event);});
+      this.el.on('click', '.change-pic-btn', function() {that.changePicture(event);});
   };
 
   this.initialize();
@@ -30,6 +31,30 @@ var EmployeeView = function(employee) {
           function() {
               alert('Error getting location');
           });
+      return false;
+  };
+
+  this.changePicture = function(event) {
+      event.preventDefault();
+      if (!navigator.camera) {
+          app.showAlert("Camera API not supported", "Error");
+          return;
+      }
+      var options =   {   quality: 50,
+                          destinationType: Camera.DestinationType.DATA_URL,
+                          sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                          encodingType: 0     // 0=JPG 1=PNG
+                      };
+
+      navigator.camera.getPicture(
+          function(imageData) {
+              $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+          },
+          function() {
+              app.showAlert('Error taking picture', 'Error');
+          },
+          options);
+
       return false;
   };
 
